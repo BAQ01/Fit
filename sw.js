fde228d6777fbf28d4c725e6e85d3ccafc709cef
BAQ01 <baquispel@gmail.com>
2025-09-05 14:46:22 +0200

    changes

--- sw.js
+++ sw.js
@@ -1,10 +1,33 @@
 const CACHE = "teun-v1";
-const ASSETS = ["./", "./index.html"];
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const fetchPromise = fetch(req)
+        .then((res) => {
+          const resClone = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, resClone));
+          return res;
+        })
+        .catch(() => cached || Promise.reject());
+      return cached || fetchPromise;
+    })
+  );
 });
